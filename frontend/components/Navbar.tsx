415ac0ee7a3fc6d8adb62ef8a8a0e449f9d1f333
Saketh Sunil Pai <paisaketh@gmail.com>
2025-09-12 19:09:10 +0530

    Better padding for the navbat

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -8,10 +8,10 @@ export default function Navbar() {
 
   return (
     <header className="vox-header">
-      <div className="vox-container flex items-center justify-between py-4">
-        <div className="flex items-center space-x-3">
+      <div className="vox-header-container">
+        <div className="vox-header-brand">
           <div className="vox-logo">
-            <span className="text-white dark:text-stone-900 font-bold text-xl">V</span>
+            <span>V</span>
           </div>
           <h1 className="vox-logo-text">Vox Dualis</h1>
         </div>
