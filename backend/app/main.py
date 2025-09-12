e48f6b1c9cf4da413d7463e4ffeddcdeb357cf46
shreyaaa-patil <v.shreyaa.patil@gmail.com>
2025-09-13 00:31:41 +0530

    commit changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,9 @@
 from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import os
 import google.generativeai as genai
-load_dotenv()# Load the .env file
-api_key = os.getenv("GOOGLE_API_KEY")# Get the API key from requirements.txt
-genai.configure(api_key=api_key)# map the api we're using for the project to the particular api key
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Say hello from Google GenAI!")#test content generation
-print(response.text)
+#commit
 class DebateRequest(BaseModel):
     topic: str
 
